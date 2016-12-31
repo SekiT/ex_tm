ab2d23565bdb99cbd9c0fe8c94ff7f1afa2b730f
Takaaki Seki <ormscrongers@gmail.com>
2016-12-31 18:35:21 +0900

    Set up mix.exs and deps

--- mix.exs
+++ mix.exs
@@ -1,32 +1,50 @@
 defmodule TuringMachine.Mixfile do
   use Mix.Project
 
+  @github_url "https://github.com/SekiT/ex_tm"
+
   def project do
-    [app: :ex_tm,
-     version: "0.1.0",
-     elixir: "~> 1.3",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :ex_tm,
+      version: "0.1.0",
+      elixir: "~> 1.3",
+      name: "ex_tm",
+      description: description(),
+      package: package(),
+      source_url: @github_url,
+      deps: deps(),
+      docs: [extras: ["README.md"]],
+      dialyzer: [plt_add_deps: :transitive],
+      test_coverage: [tool: ExCoveralls],
+    ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [applications: []]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:ex_doc     , "~> 0.14.5", only: :dev },
+      {:dialyxir   , "~> 0.4.1" , only: :dev },
+      {:credo      , "~> 0.5.3" , only: :dev },
+      {:meck       , "~> 0.8.4" , only: :test},
+      {:excoveralls, "~> 0.5.7" , only: :test},
+    ]
+  end
+
+  defp description do
+    """
+    Turing machine simulator in Elixir.
+    """
+  end
+
+  defp package do
+    [
+      name: :ex_tm,
+      licenses: ["WTFPL"],
+      maintainers: ["Takaaki Seki"],
+      links: %{"GitHub" => @github_url},
+    ]
   end
 end
