a79d6639387b683d2d2ec486709641b0767e5bb6
Takaaki Seki <ormscrongers@gmail.com>
2017-01-02 20:52:47 +0900

    Version 1.0.0

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule TuringMachine.Mixfile do
   def project do
     [
       app: :ex_tm,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.3",
       name: "ex_tm",
       description: description(),
